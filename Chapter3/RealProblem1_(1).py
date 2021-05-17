b2a7fd3bb7013bf5912a35b3a2b5a37b8b9d2c9c
sksy368 <75062273+sksy368@users.noreply.github.com>
2021-05-17 19:31:48 +0900

    Chapter3

---
+++ Chapter3/RealProblem1_(1).py
@@ -0,0 +1,15 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+result = 0
+for num in range(m):
+    if((num != 0) and ((num + 1) % k == 0)):
+        result += second
+    else:
+        result += first
+
+print(result)
