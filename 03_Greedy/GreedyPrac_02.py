30f0e9ca22b16963cf2c8c9d05c2f15d5571e643
holmir97 <holmir97@gmail.com>
2021-05-21 01:52:11 +0900

    Greedy Prac

---
+++ 03_Greedy/GreedyPrac_02.py
@@ -0,0 +1,13 @@
+N, M, K = map(int, input().split())
+
+arr = list(map(int, input().split()))
+arr.sort(reverse=True)
+
+a = arr[0]
+b = arr[1]
+
+quot = M // (K + 1)
+remain = M % (K + 1)
+
+sum = a * K * quot + b * quot + a * remain
+print(sum)
