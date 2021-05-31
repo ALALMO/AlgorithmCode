748772b24d39e325337ae4b7edd21866f717ea94
sangwon514 <aq0620@naver.com>
2021-06-01 01:52:29 +0900

    이것이 코딩 테스트다 chapter4

--- Chapter4/practice2.py
+++ Chapter4/practice2.py
@@ -18,25 +18,23 @@ turn_count = 0
 
 while(True):
     #시작하자마자 방향을 돌림
-    d -= 1
-    if d == -1:
-        d = 3
+    d = (3 + d) % 4
+
     na = a + da[d]
     nb = b + db[d]
-    print(f"na : {na} nb : {nb}")
 
+    #0일대 이동 아니면 턴
     if minimap[na][nb] == 0:
         count += 1
         minimap[na][nb] = 2
         a = na
         b = nb
         turn_count = 0
-        print('move')
         continue
     else:
         turn_count += 1
 
-    print(f"turn_count {turn_count}")
+    #턴 카운트가 4면 뒤로갈지 나갈지 판정
     if turn_count == 4:
         na = a - da[d]
         nb = b - db[d]
