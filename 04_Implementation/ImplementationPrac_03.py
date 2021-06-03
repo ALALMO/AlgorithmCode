4eb89bc861a598b7470a64191a19faf01ee8c484
holmir97 <holmir97@gmail.com>
2021-06-04 02:16:46 +0900

    feat: Implementation Prac

--- 04_Implementation/ImplementationPrac_03.py
+++ 04_Implementation/ImplementationPrac_03.py
@@ -2,9 +2,10 @@ start = input()
 x = int(start[1])
 y = int(ord(start[0])) - int(ord('a')) + 1
 
-moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
 
+moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
 cnt = 0
+
 for move in moves:
     if 1 <= x + move[0] <= 8 and 0 < y+move[1] < 9:
         cnt += 1
